d02c404394c5a55ea0ae3228f1a9b14dfcde80f2
mateomcallister <mateomcallisterschool@gmail.com>
2025-12-11 13:22:02 -0500

    refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Libre_Baskerville, Crimson_Pro } from 'next/font/google'
 import "./globals.css"
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 const libreBaskerville = Libre_Baskerville({
   subsets: ['latin'],
