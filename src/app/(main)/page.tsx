8680708eed67fc9b488961d9aae537f410ff770f
mateomcallister <mateomcallisterschool@gmail.com>
2026-04-15 17:38:15 -0400

    metadata change for SEO

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import HomeContent from "./HomeContent"
 
 export const metadata = {
   title: 'Alpha Kappa Psi',
-  description: 'Professional business fraternity at the University of Florida.',
+  description: 'The official website for the Alpha Phi chapter of the oldest professional business fraternity in the nation, Alpha Kappa Psi. Established in 1926, our chapter has been at the University of Florida for over 100 years!',
   icons: {
     icon: '/akp_emblem.png',
   },
