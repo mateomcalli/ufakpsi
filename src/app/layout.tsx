8af4a9b66e8d3514a60edf98ff1d36c893e61539
mateomcallister <mateomcallisterschool@gmail.com>
2026-06-25 02:53:18 -0400

    remove breakpoint indicator on live deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ const Layout = ({children}: Readonly<{children: React.ReactNode}>) => {
   return (
     <html className={`${libreBaskerville.variable} ${crimsonPro.variable} ${nothingYouCouldDo.variable} ${ibmPlexSans.variable} ${merriweather.variable}`} lang='en'>
       <body className="bg-cream overflow-x-hidden">
-        <BreakpointIndicator/>
+        {/* <BreakpointIndicator/> */}
         <Analytics />
         <SpeedInsights />
         <Navbar/>
